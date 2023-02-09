dd118eafea4d328ab46ffe231faecdd2053042d6
Edgar-MP <edgarstreet03@gmail.com>
2023-02-09 10:45:12 +0100

    Initial commit

---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,75 @@
+import { createContext, useState } from "react";
+import {
+  calcularMarca,
+  obtenerDiferenciaYear,
+  calcularPlan,
+  formatearDinero,
+} from "../helpers";
+
+const CotizadorContext = createContext();
+
+const CotizadorProvider = ({ children }) => {
+  const [datos, setDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
+
+  const [error, setError] = useState("");
+  const [resultado, setResultado] = useState(0);
+  const [cargando, setCargando] = useState(false);
+
+  const handleChangeDatos = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const cotizarSeguro = () => {
+
+    /** Precio base **/
+    let resultado = 200;
+
+    /** Años **/
+    const diferencia = obtenerDiferenciaYear(datos.year);
+
+    resultado -= resultado * diferencia * 0.03;
+
+    /** Procedencia **/
+    resultado *= calcularMarca(datos.marca);
+
+    /** Tipo **/
+    resultado *= calcularPlan(datos.plan);
+
+    /** Formatear dinero **/
+    resultado = formatearDinero(resultado);
+
+    setCargando(true);
+
+    setTimeout(() => {
+      setResultado(resultado);
+      setCargando(false);
+    }, 3000);
+  };
+
+  return (
+    <CotizadorContext.Provider
+      value={{
+        datos,
+        handleChangeDatos,
+        error,
+        setError,
+        cotizarSeguro,
+        resultado,
+        cargando,
+      }}
+    >
+      {children}
+    </CotizadorContext.Provider>
+  );
+};
+
+export { CotizadorProvider };
+
+export default CotizadorContext;
